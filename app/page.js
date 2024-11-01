8160b9e4d169a53a4cc08b75dfea43bfdbc86996
KarolKozikowski <134882954+KarolKozikowski@users.noreply.github.com>
2024-11-01 20:06:17 +0100

    Sorting

--- app/page.js
+++ app/page.js
@@ -11,6 +11,11 @@ export default function Home() {
     const [category, setCategory] = useState(()=>{return "Category"});
     const changeCategory = (newCategory)=>{newCategory.target.value==="All" ? setCategory("Category") : setCategory(newCategory.target.value);}
 
+    const [sort, setSort] =useState(()=>{return "DateAsc"});
+    const changeSortToTitle = ()=>{sort==="Title" ? setSort("TitleRev") : setSort("Title");}
+    const changeSortToAmount = () => {sort==="AmountAsc" ? setSort("AmountDesc") : setSort("AmountAsc");}
+    const changerSortToDate = () => {sort==="DateAsc" ? setSort("DateDesc") : setSort("DateAsc");}
+
     function displayCategories(){
         let categories = [];
         Data.map((record)=>{
@@ -24,26 +29,49 @@ export default function Home() {
             );
         });
     }
-    const displayPaymentHistory = Data.map((record, index)=>{
-        function display(){
-            return (
-                <tr onClick={()=>openMenu(record)} key={index}>
-                    <td>{record.title}</td>
-                    <td>{record.amount}zł</td>
-                    <td>{record.category}</td>
-                    <td>{record.date}</td>
-                </tr>
-            );
+    function displayPaymentHistory() {
+        let recordArray = Data.map((record)=>{
+            return record
+        });
+        switch(sort){
+            case "Title":
+                recordArray.sort((a, b) => a.title.localeCompare(b.title));
+                break;
+            case "TitleRev":
+                recordArray.sort((a, b) => b.title.localeCompare(a.title));
+                break;
+            case "AmountAsc":
+                recordArray.sort((a, b) => a.amount-b.amount);
+                break;
+            case "AmountDesc":
+                recordArray.sort((a, b) => b.amount-a.amount);
+                break;
+            case "DateAsc":
+                recordArray.sort((a, b) => b.date.localeCompare(a.date));
+                break;
+            case "DateDesc":
+                recordArray.sort((a, b) => a.date.localeCompare(b.date));
+                break;
         }
-        if(category==="Category"){
-            return display();
-        }
-        else{
-            if(record.category===category){
+        return recordArray.map((record, index) => {
+            function display() {
+                return (
+                    <tr onClick={() => openMenu(record)} key={index}>
+                        <td>{record.title}</td>
+                        <td>{record.amount}zł</td>
+                        <td>{record.category}</td>
+                        <td>{record.date}</td>
+                    </tr>
+                );
+            }
+            if (category === "Category") {
                 return display();
             }
-        }
-    });
+            else if (record.category === category) {
+                return display();
+            }
+        });
+    }
     return (
         <div>
             <title>My Payments</title>
@@ -51,18 +79,18 @@ export default function Home() {
             <table>
                 <thead>
                     <tr>
-                        <td>Title</td>
-                        <td>Amount</td>
+                        <td onClick={changeSortToTitle} className={sort==="Title" || sort==="TitleRev" ? "color" : "highlight"}>Title</td>
+                        <td onClick={changeSortToAmount} className={sort==="AmountAsc" || sort==="AmountDesc" ? "color" : "highlight"}>Amount</td>
                         <td><select defaultValue={category} onChange={changeCategory}>
                                 <option value="Category" hidden>Category</option>
                                 <option value="All">All</option>
                                 {displayCategories()}
                             </select></td>
-                        <td>Date</td>
+                        <td onClick={changerSortToDate} className={sort==="DateAsc" || sort==="DateDesc" ? "color" : "highlight"}>Date</td>
                     </tr>
                 </thead>
                 <tbody>
-                    {displayPaymentHistory}
+                    {displayPaymentHistory()}
                 </tbody>
             </table>{
             selectedCharge && (
